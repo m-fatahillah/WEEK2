5459909a9c32d66a85b30af0b6327f06472aa6f9
m-fatahillah <fatahcrown789@gmail.com>
2026-02-24 21:11:11 +0700

    praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+return 'Hello World';
+});
+
+Route::get('/world', function () {
+return 'World';
+});
+
+Route::get('/wellcome',function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about',function () {
+    return 'Nama : Muhammad Fatahillah Athabrani <br>
+            Nim  : 244107020121 ';
+});
+
+Route::get('/user/{name}', function ($name) {
+return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+return 'Nama saya '.$name;
+});
